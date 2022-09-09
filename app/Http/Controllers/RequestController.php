e14f185092b31d1f19bc2a1261e94a08ad7456a7
dudosyka <a.hatson22@gmail.com>
2022-09-09 21:23:13 +0300

    Files uploading

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -4,11 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\RequestModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class RequestController extends Controller
 {
     public function create(Request $request) {
-        return response()->json(RequestModel::createNew($request->all(), $request->user()));
+        if ($request->hasFile('photo_before')) {
+            $file = $request->file('photo_before');
+            $ext = explode(".", $file->getClientOriginalName())[count(explode(".", $file->getClientOriginalName())) - 1];
+            $new_name = Str::random(32).".".$ext;
+            $file->move(public_path() . '/images', $new_name);
+            $data = $request->all();
+            $data['photo_before'] = $new_name;
+            return response()->json(RequestModel::createNew($data, $request->user()));
+        } else {
+            return response()->json([
+                'request_create' => [
+                    'status' => 'failed',
+                    'errors' => ['photo_before' => 'required']
+                ]
+            ]);
+        }
     }
 
     public function remove(Request $request) {
@@ -16,7 +32,27 @@ class RequestController extends Controller
     }
 
     public function updateStatus(Request $request) {
-        return response()->json(RequestModel::updateStatus($request->all(), $request->user()));
+        $data = $request->all();
+        if ($data['status'] == 'done') {
+            if ($request->hasFile('photo_after')) {
+                $file = $request->file('photo_after');
+                $ext = explode(".", $file->getClientOriginalName())[count(explode(".", $file->getClientOriginalName())) - 1];
+                $new_name = Str::random(32).".".$ext;
+                $file->move(public_path() . '/images', $new_name);
+                $data['photo_after'] = $new_name;
+                return response()->json(RequestModel::updateStatus($data, $request->user()));
+            }
+            else {
+                return response()->json([
+                    'request_update' => [
+                        'status' => 'failed',
+                        'errors' => ['photo_after' => 'required']
+                    ]
+                ]);
+            }
+        } else {
+            return response()->json(RequestModel::updateStatus($data, $request->user()));
+        }
     }
 
     public function getAll() {
